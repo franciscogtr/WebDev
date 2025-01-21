d7fb4ebb3ce1770be0265609fc3239bf3073e075
franciscogtr <franciscodesousamatos@outlook.com>
2025-01-21 08:12:49 -0300

    Inserindo Index

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reserva de Ambientes</title>
+</head>
+<body>
+    <header>
+        <h1>Reserva de Ambientes</h1>
+    </header>
+    <main>
+        <h2>Ambientes</h2>
+    </main>
+    <footer>
+        <p>&copy; Web Dev 2024</p>
+    </footer>
+</body>
+</html>
