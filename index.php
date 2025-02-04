a2330a9b126b16e7527941a71fc4910f2a14bb96
Lucas Emanuel <emanuellucas72902@gmail.com>
2025-02-04 19:06:04 -0300

    mostrar ambientes em index

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+    
+    require_once 'class/rb.php';
+    include 'inc/conexaoBD.inc.php';
+
+    $salas = R::find( 'ambiente', ' tipo LIKE ? ', [ 'sal' ] );
+    $labs = R::find( 'ambiente', ' tipo LIKE ? ', [ 'lab' ] );
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -21,7 +31,34 @@
 
         <h3>Salas</h3>
 
+        <?php
+
+        $heredoc = <<<CARDINDEX
+
+         <div class='cardindex'>
+            <div>
+                <img src="img/%s" alt="Imagem do Ambiente">
+            </div>
+            <div>
+                <p>%s</p>
+            </div>
+        </div>
+
+CARDINDEX;
+            if (count($salas) > 0) {
+                foreach ($salas as  $sala) {
+                    printf($heredoc, $sala->imagem,$sala->nome);
+                }
+            }
+        ?>
         <h3>Laboratórios</h3>
+        <?php
+         if (count($salas) > 0) {
+            foreach ($labs as  $lab) {
+                printf($heredoc, $lab->imagem,$lab->nome);
+            }
+        }
+        ?>
     </main>
     <footer>
        <?php include 'inc/rodape.inc.php'?>
