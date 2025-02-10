2456c4fb5c993b53ccf61747145236bf1473f0e4
franciscogtr <franciscodesousamatos@gmail.com>
2025-02-10 19:11:52 -0300

    CSS de calendaário e card Index

--- calendario.php
+++ calendario.php
@@ -8,76 +8,17 @@
     <link rel="stylesheet" href="stylesheet.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
     <style>
-        #formMes{
-    display: flex;
-            justify-content: center; /* Alinha horizontalmente no centro */
-            align-items:baseline; /* Alinha verticalmente no topo */
-            margin: 0 auto;
-            
-
-}
-table {
-    justify-content: center; /* Alinha horizontalmente no centro */
-    border: solid #000000;
-    text-align: center;
-    margin: 0 auto;
-    border-collapse: separate;
-}
-
-td {
-    padding-left: 5px;
-    padding-right: 5px;
-    padding-top: 2px;
-    padding-bottom: 2px;
-    font-weight: bold;
-    
-
-}
-
-th {
-    color: #F2F2F2;
-    background-color: #3876F2;
-    padding-left: 5px;
-    padding-right: 5px;
-    padding-top: 2px;
-    padding-bottom: 2px;
-    
-
-}
-
-
-#calendario {
-    color: black;
-    text-decoration: none;
-}
-
-#calendario:hover {
-    color: #3876F2;
-}
-
-tr:nth-child(even) {
-    background-color: #C7C7C7;
-}
-
-#data{
-    text-align: center;
-}
-
-input[type="month"] {
-    background-color: #3876F2;
-    color: #F2F2F2;
-    border: 2px solid #333;
-    padding: 8px;
-    border-radius: 5px;
-}
+        
     </style>
 </head>
 
 <body>
     <header>
         <?php
-            //include 'inc/cabecalho.inc.php';
+        include 'inc/cabecalho.inc.php';
         ?>
+
+        <p><a href="index.php" class="voltar">Home</a> </p>
     </header>
 
     <main>
@@ -102,51 +43,49 @@ input[type="month"] {
             $dataCompleta =  new DateTime($data);
 
             $strdata = $_GET['mes'];
-                        $data = new DateTime($strdata);
-                        // echo $data->format('m');
+            $data = new DateTime($strdata);
+            // echo $data->format('m');
 
-                        switch ($data->format('m')) {
-                            case '1':
-                                $mes = 'Janeiro';
-                                break;
-                                case '2':
-                                    $mes = 'Fevereiro';
-                                    break;
-                                    case '3':
-                                        $mes = 'Março';
-                                        break;
-                                        case '4':
-                                            $mes = 'Abril';
-                                            break;
-                                            case '5':
-                                                $mes = 'Maio';
-                                                break;
-                                                case '6':
-                                                    $mes = 'Junho';
-                                                    break;
-                                                    case '7':
-                                                        $mes = 'Julho';
-                                                        break;
-                                                        case '8':
-                                                            $mes = 'Agosto';
-                                                            break;
-                                                            case '9':
-                                                                $mes = 'Setembro';
-                                                                break;
-                                                                case '10':
-                                                                    $mes = 'Outubro';
-                                                                    break;
-                                                                    case '11':
-                                                                        $mes = 'Novembro';
-                                                                        break;
-                                                                        case '12':
-                                                                            $mes = 'Dezembro';
-                                                                            break;
-                                                                    
-                            
-                        }
+            switch ($data->format('m')) {
+                case '1':
+                    $mes = 'Janeiro';
+                    break;
+                case '2':
+                    $mes = 'Fevereiro';
+                    break;
+                case '3':
+                    $mes = 'Março';
+                    break;
+                case '4':
+                    $mes = 'Abril';
+                    break;
+                case '5':
+                    $mes = 'Maio';
+                    break;
+                case '6':
+                    $mes = 'Junho';
+                    break;
+                case '7':
+                    $mes = 'Julho';
+                    break;
+                case '8':
+                    $mes = 'Agosto';
+                    break;
+                case '9':
+                    $mes = 'Setembro';
+                    break;
+                case '10':
+                    $mes = 'Outubro';
+                    break;
+                case '11':
+                    $mes = 'Novembro';
+                    break;
+                case '12':
+                    $mes = 'Dezembro';
+                    break;
+            }
 
-                        echo '<h1 id="data">' . $mes . ' de ' . $data->format('Y') . '</h1>';
+            echo '<h1 id="data">' . $mes . ' de ' . $data->format('Y') . '</h1>';
 
         ?>
             <table>
--- index.php
+++ index.php
@@ -12,42 +12,47 @@ $labs = R::find('ambiente', ' tipo LIKE ? ', ['lab']);
 <html lang="pt-br">
 
 <head>
-    <meta charset="UTF-8" http-equiv="refresh" content="5">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reserva de Ambientes</title>
     <link rel="stylesheet" href="stylesheet.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
 </head>
+<style>
+    
+
+    
+</style>
 
 <body>
     <header>
-    <?php
-            include 'inc/cabecalho.inc.php'
-            ?>
+        <?php
+        include 'inc/cabecalho.inc.php'
+        ?>
         <nav>
             <a class="aheader" href="calendario.php">Reservar</a>
             <a class="aheader" href="minhassreservas.php">Minhas Reservas</a>
             <?php if (isset($_SESSION['admin']) && $_SESSION['admin'] === 'on'): ?>
-                    
-                <select id="cadastrar" onchange="redirecionarPagina()">
-                <option value="">Cadastrar</option>
-                <option value="cadastroambiente.php">Ambiente</option>
-                <option value="cadastrousuario.php">Usuário</option>
-            </select>
-            <script>
-                function redirecionarPagina() {
-                    var select = document.getElementById("cadastrar");
-                    var valor = select.value;
-                    if (valor) {
-                        window.location.href = valor; // Redireciona para a página selecionada
-                    }
-                }
-            </script>
 
-                <?php endif; ?>
-                <!-- Caixa seletora oculta para alinhamento do cabeçalho -->
-                <select style="color:#f2f2f2"></select> 
-            
+                <select id="cadastrar" onchange="redirecionarPagina()">
+                    <option value="">Cadastrar</option>
+                    <option value="cadastroambiente.php">Ambiente</option>
+                    <option value="cadastrousuario.php">Usuário</option>
+                </select>
+                <script>
+                    function redirecionarPagina() {
+                        var select = document.getElementById("cadastrar");
+                        var valor = select.value;
+                        if (valor) {
+                            window.location.href = valor; // Redireciona para a página selecionada
+                        }
+                    }
+                </script>
+
+            <?php endif; ?>
+            <!-- Caixa seletora oculta para alinhamento do cabeçalho -->
+            <select style="color:#f2f2f2"></select>
+
             <a class="aheader" href="sobre.php">Sobre</a></p>
 
         </nav>
@@ -55,36 +60,36 @@ $labs = R::find('ambiente', ' tipo LIKE ? ', ['lab']);
 
 
     <main>
-        <h2>Ambientes</h2>
         
 
-        <h3>Salas</h3>
+
+        <h1>Salas</h1>
 
         <?php
 
         $heredoc = <<<CARDINDEX
 
-         <div class='cardindex'>
-            <div>
+         <div class='cardIndex'>
+            <div class='item'> 
                 <img src="img/%s" alt="Imagem do Ambiente">
             </div>
-            <div>
-                <p>%s</p>
-            </div>
+            <p class='pCard'>%s</p>
+                <p class='pCard'><a class='aCard' href="calendario.php?ambiente=%s">Reservar</a></p>
         </div>
 
 CARDINDEX;
+
         if (count($salas) > 0) {
             foreach ($salas as  $sala) {
-                printf($heredoc, $sala->imagem, $sala->nome);
+                printf($heredoc, $sala->imagem, $sala->nome, $sala->nome);
             }
         }
         ?>
-        <h3>Laboratórios</h3>
+        <h1>Laboratórios</h1>
         <?php
         if (count($salas) > 0) {
             foreach ($labs as  $lab) {
-                printf($heredoc, $lab->imagem, $lab->nome);
+                printf($heredoc, $lab->imagem, $lab->nome, $lab->nome);
             }
         }
         ?>
