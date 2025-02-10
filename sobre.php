ea1d8ff174cf7b50f73100a92abbb7afb8007233
Samue1x <samuelmtct@gmail.com>
2025-02-10 12:08:20 -0300

    atualização sobre

--- sobre.php
+++ sobre.php
@@ -1,20 +1,45 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        img {
+            border: solid black 1px;
+            border-radius: 50%;
+            /* size: 10px; */
+            height: 20%;
+            width: 20%;
+        }
+    </style>
 </head>
+
 <body>
-    
+    <header>
+        <?php
+        include 'inc/cabecalho.inc.php'
+        ?>
+    </header>
+    <main>
+        
+        <h2>Sobre</h2>
+
+        <h3><b>Feito por: </b></h3> <br>
+        <p><img src="img/analuiza.png" alt="Ana Luiza"><br>Nome: Ana Luiza Alves Viera</p>
+        <p>Email: alav@aluno.ifnmg.edu.br</p>
+        <p><img src="img/francisco.jpg" alt="foto Francisco"><br>Nome: Francisco José Geraldo Matos de Sousa</p>
+        <p>Email: fjgms@aluno.ifnmg.edu.br</p>
+        <p> <img src="img/samuel.jpg" alt="Foto Samuel"><br>Nome: Samuel Motta de Castro</p>
+        <p>Email: smc11@aluno.ifnmg.edu.br</p>
+
+
+    </main>
+    <footer>
+        <?php
+        include 'inc/rodape.inc.php'
+        ?>
+    </footer>
 </body>
-
-<?php include 'inc/cabecalho.inc.php'; ?>
-<h1>Sobre a Equipe</h1>
-<article>
-    <img src="assets/team1.jpg" alt="Membro 1" class="circular">
-    <h2>Nome do Membro 1</h2>
-    <p>Email: membro1@example.com</p>
-</article>
-
 </html>
