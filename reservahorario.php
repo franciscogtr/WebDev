61adbbee61764e5d78a0df30d02569bbd69bee1c
plca87 <c067581@gmail.com>
2025-02-11 11:01:34 -0300

    criação reservahorario.php

---
+++ reservahorario.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>reserva horario</title>
+</head>
+
+<body>
+	<header>
+		<?php include 'inc/cabecalho.inc.php' ?>
+	</header>
+	<main>
+		<?php
+
+		require_once 'class/rb.php';
+		include 'inc/conexaoBD.inc.php';
+		?>
+
+		<footer>
+			<?php include 'inc/rodape.inc.php' ?>
+		</footer>
+</body>
+
+</html>
