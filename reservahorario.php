20d334adda32393ec206fd106b9dbebd9544ca2c
ErisSilvar <erisemanoelrs@gmail.com>
2025-02-11 17:07:51 -0300

    Inserindo css em reservahorario.php

--- reservahorario.php
+++ reservahorario.php
@@ -5,11 +5,15 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>reserva horario</title>
+	<link rel="stylesheet" href="stylesheet.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
 	<header>
 		<?php include 'inc/cabecalho.inc.php' ?>
+
+		<p><a href="index.php" class="voltar">Home</a> </p>
 	</header>
 	<main>
 
