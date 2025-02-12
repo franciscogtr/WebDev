879fb9edee93a1cbcfbf739fc70db326d01ead4c
Samue1x <samuelmtct@gmail.com>
2025-02-12 12:04:10 -0300

    tabela adicionada em minhas reservas

--- minhasreservas.php
+++ minhasreservas.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Minhas reservas</title>
+    <link rel="stylesheet" href="stylesheet.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -23,13 +25,48 @@
         require_once 'class/rb.php';
         include 'inc/conexaoBD.inc.php';
 
-        $todasReservas = R::find('reserva', ' nome LIKE ? ', [$_SESSION['nome']]);
+       
 
-        foreach($todasReservas as $reservasfeitas){
+        $iniciotabela = <<<INICIO
+                <table>
+                    <thead>
+                        <th>Reservante</th>
+                        <th>Data da reserva</th>
+                        <th>Ambiente reservado</th>
+                        <th>Hora reservada</th>
+                    </thead>
+                    <tbody>
+INICIO;
 
+        $corpotabela = <<<CORPO
+            <tr>
+                            <td>%s</td>
+                            <td>%s</td>
+                            <td>%s</td>
+                            <td>%s</td>
+                        </tr>
+CORPO;
+
+
+        echo $iniciotabela;
+        $todasReservas = R::find('reserva', ' nome_resevante LIKE ? ', [$_SESSION['nome']]);       
+        
+        foreach ($todasReservas as $key => $value) {
+            printf(
+                $corpotabela,
+                $value->nome_resevante,
+                $value->ambiente,
+                $value->data,
+                $value->hora
+            );
         }
-    
+
+        echo "  </tbody>
+                    </table>";
         ?>
+        <p >
+
+        </p>
     </main>
     <footer>
         <?php include 'inc/rodape.inc.php' ?>
